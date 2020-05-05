79c8f36fed7ce07d0b64997e2989db59482471fa
muhsenmaqsudi <muhsenmaqsudi@gmail.com>
2020-05-05 11:00:37 +0430

    fixed dropdown width size

--- resources/views/components/dropdown.blade.php
+++ resources/views/components/dropdown.blade.php
@@ -1,5 +1,5 @@
 <div class="flex flex-wrap" x-data="dropdown()">
-    <div class="w-full px-2">
+    <div class="w-48 px-2">
         <div class="relative inline-flex align-middle w-full">
             <button @click="toggleDropdown()" x-ref="btnDropdownRef"
                     class="w-full text-white font-bold text-sm px-6 py-3 rounded shadow {{ $bgColor }} hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 font-normal px-6 py-2 rounded outline-none focus:outline-none mr-1 mb-1 capitalize transition transition-all ease-in duration-1000"
